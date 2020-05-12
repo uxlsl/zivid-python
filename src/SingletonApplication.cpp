041d7cfa8ffda06c494f2e9c8d4e91882c04be37
Trym Bremnes <trym.bremnes@zivid.com>
2020-05-12 11:27:07 +0200

    added application methods back again

--- src/SingletonApplication.cpp
+++ src/SingletonApplication.cpp
@@ -11,22 +11,20 @@ namespace ZividPython
     void wrapClass(pybind11::class_<SingletonApplication> pyClass)
     {
         pyClass.def(py::init())
-            .def("cameras", &SingletonApplication::cameras);
-//            .def("connect_camera",
-//                 py::overload_cast<const Zivid::Settings &>(&SingletonApplication::connectCamera),
-//                 py::arg("settings") = Zivid::Settings{})
-//            .def(
-//                "connect_camera",
-//                [](SingletonApplication &application,
-//                   const std::string &serialNumber,
-//                   const Zivid::Settings &settings) {
-//                    return application.connectCamera(Zivid::SerialNumber{ serialNumber }, settings);
-//                },
-//                py::arg("serial_number"),
-//                py::arg("settings") = Zivid::Settings{})
-//            .def("create_file_camera",
-//                 &SingletonApplication::createFileCamera,
-//                 py::arg("frame_file"),
-//                 py::arg("settings") = Zivid::Settings{});
+            .def("cameras", &SingletonApplication::cameras)
+            .def("connect_camera", [](SingletonApplication &application) {
+                    return application.connectCamera();
+                }
+            )
+            .def(
+                "connect_camera",
+                [](SingletonApplication &application,
+                   const std::string &serialNumber) {
+                    return application.connectCamera(Zivid::SerialNumber{ serialNumber });
+                },
+                py::arg("serial_number"))
+            .def("create_file_camera",
+                 &SingletonApplication::createFileCamera,
+                 py::arg("frame_file"));
     }
 } // namespace ZividPython
