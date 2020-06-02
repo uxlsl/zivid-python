173b025e44d984bd4c35d6602402c46a3da0e157
Trym Bremnes <trym.bremnes@zivid.com>
2020-06-02 11:55:56 +0200

    updated camera state

--- modules/_zivid/generate_files.py
+++ modules/_zivid/generate_files.py
@@ -8,10 +8,14 @@ import _zivid.generate_to_internal_settings_2d_converter
 import _zivid.generate_to_settings_2d_converter
 import _zivid.generate_frame_info
 import _zivid.generate_to_frame_info_converter
+import _zivid.generate_camera_state
+import _zivid.generate_to_camera_state_converter
 
 
 def generate():
     pass
+    _zivid.generate_to_camera_state_converter.start_traverse()
+    # _zivid.generate_camera_state.start_traverse()
     #_zivid.generate_frame_info.start_traverse()
     # _zivid.generate_to_frame_info_converter.start_traverse()
     # _zivid.generate_to_settings_2d_converter.start_traverse()
