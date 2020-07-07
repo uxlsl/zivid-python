b0ce5969e13d939c4ec810b1248755d0c4dedcf5
Trym Bremnes <trym.bremnes@gmail.com>
2020-07-07 12:43:12 +0200

    added more tests

--- test/test_frame_info.py
+++ test/test_frame_info.py
@@ -17,3 +17,14 @@ def test_software_version(frame_info):
     assert isinstance(software_version, zivid.frame_info.FrameInfo.SoftwareVersion)
     assert software_version.core
     assert isinstance(software_version.core, str)
+
+
+def test_set_time_stamp(frame_info):
+    import zivid
+    import datetime
+
+    assert isinstance(frame_info.time_stamp, datetime.datetime)
+    assert isinstance(str(frame_info.time_stamp), str)
+    frame_info.time_stamp = datetime.datetime(1337, 1, 1)
+    assert isinstance(str(frame_info.time_stamp), str)
+    assert isinstance(frame_info.time_stamp, datetime.datetime)
