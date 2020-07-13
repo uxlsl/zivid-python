80017436b4de78c0b2380f56aca0843d78f40c37
Trym Bremnes <trym.bremnes@gmail.com>
2020-07-13 13:40:52 +0200

    updated tests

--- test/converters/test_convert_suggest_settings_parameters.py
+++ test/converters/test_convert_suggest_settings_parameters.py
@@ -4,17 +4,19 @@
 def test_to_internal_suggest_settings_parameters_to_suggest_settings_parameters_modified():
     import datetime
     from zivid.capture_assistant import SuggestSettingsParameters
-    from zivid._suggest_settings_parameters_converter import (
-        to_suggest_settings_parameters,
-        to_internal_suggest_settings_parameters,
+    from zivid._capture_assistant_suggest_settings_parameters_converter import (
+        to_capture_assistant_suggest_settings_parameters,
+        to_internal_capture_assistant_suggest_settings_parameters,
     )
 
     modified_suggest_settings_parameters = SuggestSettingsParameters(
         ambient_light_frequency=SuggestSettingsParameters.AmbientLightFrequency.hz50
     )
 
-    converted_suggest_settings_parameters = to_suggest_settings_parameters(
-        to_internal_suggest_settings_parameters(modified_suggest_settings_parameters)
+    converted_suggest_settings_parameters = to_capture_assistant_suggest_settings_parameters(
+        to_internal_capture_assistant_suggest_settings_parameters(
+            modified_suggest_settings_parameters
+        )
     )
     assert modified_suggest_settings_parameters == converted_suggest_settings_parameters
     assert isinstance(converted_suggest_settings_parameters, SuggestSettingsParameters)
@@ -23,14 +25,16 @@ def test_to_internal_suggest_settings_parameters_to_suggest_settings_parameters_
 
 def test_to_internal_suggest_settings_parameters_to_suggest_settings_parameters_default():
     from zivid.capture_assistant import SuggestSettingsParameters
-    from zivid._suggest_settings_parameters_converter import (
-        to_suggest_settings_parameters,
-        to_internal_suggest_settings_parameters,
+    from zivid._capture_assistant_suggest_settings_parameters_converter import (
+        to_capture_assistant_suggest_settings_parameters,
+        to_internal_capture_assistant_suggest_settings_parameters,
     )
 
     default_suggest_settings_parameters = SuggestSettingsParameters()
-    converted_suggest_settings_parameters = to_suggest_settings_parameters(
-        to_internal_suggest_settings_parameters(default_suggest_settings_parameters)
+    converted_suggest_settings_parameters = to_capture_assistant_suggest_settings_parameters(
+        to_internal_capture_assistant_suggest_settings_parameters(
+            default_suggest_settings_parameters
+        )
     )
     assert default_suggest_settings_parameters == converted_suggest_settings_parameters
     assert isinstance(converted_suggest_settings_parameters, SuggestSettingsParameters)
