1b71a2d1c617a6f532a0d786f94c16f91c361eb2
Trym Bremnes <trym.bremnes@gmail.com>
2020-07-13 13:39:49 +0200

    updated tests

--- test/converters/test_convert_settings_2d.py
+++ test/converters/test_convert_settings_2d.py
@@ -4,7 +4,7 @@
 def test_to_internal_settings_to_settings_modified():
     import datetime
     from zivid import Settings2D
-    from zivid._settings_2d_converter import to_settings_2d, to_internal_settings_2d
+    from zivid._settings2_d_converter import to_settings2_d, to_internal_settings2_d
 
     modified_settings = Settings2D(
         acquisitions=[
@@ -13,7 +13,7 @@ def test_to_internal_settings_to_settings_modified():
         ]
     )
 
-    converted_settings = to_settings_2d(to_internal_settings_2d(modified_settings))
+    converted_settings = to_settings2_d(to_internal_settings2_d(modified_settings))
     assert modified_settings == converted_settings
     assert isinstance(converted_settings, Settings2D)
     assert isinstance(modified_settings, Settings2D)
@@ -21,10 +21,10 @@ def test_to_internal_settings_to_settings_modified():
 
 def test_to_internal_settings_to_settings_default():
     from zivid import Settings2D
-    from zivid._settings_2d_converter import to_settings_2d, to_internal_settings_2d
+    from zivid._settings2_d_converter import to_settings2_d, to_internal_settings2_d
 
     default_settings = Settings2D()
-    converted_settings = to_settings_2d(to_internal_settings_2d(default_settings))
+    converted_settings = to_settings2_d(to_internal_settings2_d(default_settings))
     assert default_settings == converted_settings
     assert isinstance(converted_settings, Settings2D)
     assert isinstance(default_settings, Settings2D)
